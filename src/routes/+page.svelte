23148bb7e0cd1ea1b487b6fc8fd1da2b02c34a29
Matthias Wang <matthiaswang60@gmail.com>
2023-01-22 20:06:59 +0800

    Working settings dialog

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,19 @@
 <script>
     //import components from Fluent Svelte
-    import { Button, RadioButton } from "fluent-svelte";
+    import { Button, RadioButton, ContentDialog } from "fluent-svelte";
+
+    //#region Variable declarations
 
     //variable to keep track of passage type
-    let passageIsRandomWords = false;
+    let passageIsRandomWords = true;
+
+    //variable to keep track of whether the settings dialog is open or not
+    let settingsDialogIsOpen = false;
+
+    //variable to keep track of the amount of time per game that the user has selected
+    let gameTime = 30;
+
+    //#endregion
 </script>
 
 <svelte:head>
@@ -18,10 +28,38 @@
        </div>
        
        <div class="settings-btn-div">
-            <Button variant="accent">Settings</Button>
+            <!-- Settings button which opens a settings dialog -->
+            <Button variant="accent" on:click={() => (settingsDialogIsOpen = true)}>Settings</Button>
        </div>
-
     </div>
+
+     <!-- Settings dialog -->
+    <ContentDialog bind:open={settingsDialogIsOpen} title="Settings">
+        <!-- Content for the settings dialog goes here -->
+        
+        <!-- Div containing options to change the passage type -->
+        <div>
+            <h3 class="dialog-titles">Passage Type:</h3>
+            <RadioButton bind:group={passageIsRandomWords} value={true}>Random words</RadioButton>
+            <br/>
+            <RadioButton bind:group={passageIsRandomWords} value={false}>Random quote/punctuation</RadioButton>
+        </div>
+
+        <!-- Div containing options to change the amount of time -->
+        <div>
+            <h3 class="dialog-titles">Amount of time:</h3>
+            <RadioButton bind:group={gameTime} value={15}>15s</RadioButton>
+            <br/>
+            <RadioButton bind:group={gameTime} value={30}>30s</RadioButton>
+            <br/>
+            <RadioButton bind:group={gameTime} value={60}>60s</RadioButton>
+        </div>
+
+        <!-- Close button -->
+        <Button slot="footer" variant="accent" on:click={() => (settingsDialogIsOpen = false)}>Close</Button>
+    </ContentDialog>
+
+
 </body>
 
 <style lang="scss">
